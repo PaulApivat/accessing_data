1ca676751e15ee45408cd02e0337f95214ba8970
paulapivat <paul.apivat@gmail.com>
2020-07-08 13:04:00 +0700

    not getting far with setup

--- bigquery/setup.R
+++ bigquery/setup.R
@@ -1,11 +1,26 @@
 # Environment ----
+# R version 3.6.3 (2020-02-29)
+# Platform: x86_64-apple-darwin15.6.0 (64-bit)
+# Running under: macOS Catalina 10.15.5
+# RStudio Version 1.2.5042
 
 
 # Installation ----
-
+install.packages("bigrquery")
 
 
 # Usage ----
 
+# Low-level API
+library(bigrquery)
 
 
+# DBI
+library(DBI)
+
+con <- dbConnect(
+    bigrquery::bigquery(),
+    project = "jobsbot-276604",
+    dataset = "smartjob",
+    billing = jobsbot-276604
+)
