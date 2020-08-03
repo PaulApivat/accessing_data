b0943612a946b062b70c1d6949d6c4a1f447e277
paulapivat <paul.apivat@gmail.com>
2020-08-03 10:31:49 +0700

    add two queries to bigquery setup.R

--- bigquery/setup.R
+++ bigquery/setup.R
@@ -5,6 +5,43 @@
 # RStudio Version 1.2.5042
 
 
+# BigQuery platform (not Export yet) ----
+
+# Identify Primary Keys in each table _sdc_primary_keys
+
+# mixpanel_export distinct_id
+# mixpanel_export _rjm_record_hash
+# mixpanel_export event
+# mixpanel_export time
+# mixpanel_engage mp_reserved_distinct_id
+# mixpanel_funnels funnel_id
+# mixpanel_funnels date
+
+
+# 3 Distinct funnel_id from mixpael_funnels
+SELECT DISTINCT(funnel_id) 
+FROM `jobsbot-276604.jobsbot_mixpanel.mixpanel_funnels` 
+LIMIT 200
+
+
+# BigQuery: What basic data would be useful? ----
+
+# Count of events  from mixpanel_export
+# job listing, get job details, share job, got to smartjob, contact employer
+SELECT event, COUNT(event) FROM `jobsbot-276604.jobsbot_mixpanel.mixpanel_export` 
+GROUP BY event
+LIMIT 700
+
+# Count gender from mixpanel_engage
+# female (115) male (51)
+SELECT gender, COUNT(gender) FROM `jobsbot-276604.jobsbot_mixpanel.mixpanel_engage` 
+GROUP BY gender
+LIMIT 200
+
+# Next Step ----
+# Explore visualizing on Google Data Studio
+
+# Connect R to BigQuery ----
 # Installation ----
 install.packages("bigrquery")
 
