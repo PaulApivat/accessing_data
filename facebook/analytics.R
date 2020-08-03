f78e47d3d1b26f71c0b0bd709d2ae5e6c3cbbee1
paulapivat <paul.apivat@gmail.com>
2020-08-03 09:23:13 +0700

    comments and next step, access facebook analytics by request to be added to Saku Admin

--- facebook/analytics.R
+++ facebook/analytics.R
@@ -3,3 +3,11 @@
 # Platform: x86_64-apple-darwin15.6.0 (64-bit)
 # Running under: macOS Catalina 10.15.5
 # RStudio Version 1.2.5042
+
+# Setting Up: Facebook Analytics ----
+
+# Step 1: Create App: saku_analytics
+# Step 1a: Add 'Analytics' to App
+# Step 1b: Choose platform for this app (Web)
+
+# Tentative solution: Request access to Saku admin page for faster API access
